458d6e131058d29023da88c0a5411f7ad56738c1
Андрей <40750040+AndrewStartcev@users.noreply.github.com>
2022-06-23 22:17:13 +0800

    first commit

---
+++ app/assets/js/main.js
@@ -0,0 +1,123 @@
+// ======== Скрипты ==========================
+
+// ======== Маска для телефона ===============
+document.addEventListener("DOMContentLoaded", function () {
+    var phoneInputs = document.querySelectorAll('input[type="tel"]');
+
+    var getInputNumbersValue = function (input) {
+        return input.value.replace(/\D/g, '');
+    }
+
+    var onPhonePaste = function (e) {
+        var input = e.target,
+            inputNumbersValue = getInputNumbersValue(input);
+        var pasted = e.clipboardData || window.clipboardData;
+        if (pasted) {
+            var pastedText = pasted.getData('Text');
+            if (/\D/g.test(pastedText)) {
+                input.value = inputNumbersValue;
+                return;
+            }
+        }
+    }
+
+    var onPhoneInput = function (e) {
+        var input = e.target,
+            inputNumbersValue = getInputNumbersValue(input),
+            selectionStart = input.selectionStart,
+            formattedInputValue = "";
+
+        if (!inputNumbersValue) {
+            return input.value = "";
+        }
+
+        if (input.value.length != selectionStart) {
+            if (e.data && /\D/g.test(e.data)) {
+                input.value = inputNumbersValue;
+            }
+            return;
+        }
+
+        if (["7", "8", "9"].indexOf(inputNumbersValue[0]) > -1) {
+            if (inputNumbersValue[0] == "9") inputNumbersValue = "7" + inputNumbersValue;
+            var firstSymbols = (inputNumbersValue[0] == "8") ? "8" : "+7";
+            formattedInputValue = input.value = firstSymbols + " ";
+            if (inputNumbersValue.length > 1) {
+                formattedInputValue += '(' + inputNumbersValue.substring(1, 4);
+            }
+            if (inputNumbersValue.length >= 5) {
+                formattedInputValue += ') ' + inputNumbersValue.substring(4, 7);
+            }
+            if (inputNumbersValue.length >= 8) {
+                formattedInputValue += '-' + inputNumbersValue.substring(7, 9);
+            }
+            if (inputNumbersValue.length >= 10) {
+                formattedInputValue += '-' + inputNumbersValue.substring(9, 11);
+            }
+        } else {
+            formattedInputValue = '+' + inputNumbersValue.substring(0, 16);
+        }
+        input.value = formattedInputValue;
+    }
+    var onPhoneKeyDown = function (e) {
+        var inputValue = e.target.value.replace(/\D/g, '');
+        if (e.keyCode == 8 && inputValue.length == 1) {
+            e.target.value = "";
+        }
+    }
+    for (var phoneInput of phoneInputs) {
+        phoneInput.addEventListener('keydown', onPhoneKeyDown);
+        phoneInput.addEventListener('input', onPhoneInput, false);
+        phoneInput.addEventListener('paste', onPhonePaste, false);
+    }
+});
+
+//======== Аккардион =================
+$.fn.accordion = function (options) {
+    var settings = $.extend({
+        autoCollapse: false
+    }, options);
+
+    var
+        $accordion = $(this),
+        blockName = $accordion.attr('data-block'),
+        $items = $('.' + blockName + '__item', $accordion);
+
+    $accordion.delegate('.' + blockName + '__title', 'click', triggerAccordion);
+
+    function triggerAccordion() {
+        var
+            $that = $(this),
+            $parent = $that.parent(),
+            $content = $parent.children('.' + blockName + '__content'),
+            isOpen = $that.hasClass('js-accordion--open'),
+            autoCollapse = true,
+            contentHeight = $content.prop('scrollHeight');
+
+        if (isOpen) {
+            $that.removeClass('js-accordion--open');
+            $parent.removeClass('js-accordion--open');
+            $content.css('height', contentHeight);
+            setTimeout(function () {
+                $content.removeClass('js-accordion--open').css('height', '');
+            }, 4);
+        } else {
+            if (settings.autoCollapse) {
+                //auto collapse open accordions
+            }
+            $('.accordion__title').removeClass('js-accordion--open');
+            $('.accordion__item').removeClass('js-accordion--open');
+            $('.accordion__content').css('height', $('.accordion__content').prop('scrollHeight'));
+            $('.accordion__content').removeClass('js-accordion--open').css('height', '');
+
+            $that.addClass('js-accordion--open');
+            $parent.addClass('js-accordion--open');
+            $content.addClass('js-accordion--open').css('height', contentHeight).one('webkitTransitionEnd', event, function () {
+                if (event.propertyName === 'height') {
+                    $(this).css('height', '');
+                }
+            });
+        }
+    }
+};
+$('#faq-accordion').accordion();
---
+++ readme.md
@@ -0,0 +1,67 @@
+# Базовый шаблон для верстки
+
+Базовый шаблон для верстки с настроенной gulp сборкой
+
+### Используемые технологии
+
+Html, JavaScript, Gulp, Node, scss
+
+### Установка
+
+Рекомендуемая среда разработки - VS Code.
+Предварительно установите Node.js (https://nodejs.org/en/) и gulp (https://gulpjs.com/).
+Затем в консоли пропишите:
+
+```bash
+npm i
+```
+
+### Запуск
+
+```bash
+cd gulp
+```
+
+```bash
+gulp
+```
+
+### Настройка контейнера
+
+Переменные:
+
+> app/scss/\_base.scss
+
+```
+$maxWidth: 1920; // Не меняем.
+$maxWidthContainer: 1200; // Максимальный размер контейнера
+$md1: $maxWidthContainer + 10; // Адаптив контейнера, когда экран меньше чем 1200
+$md2: 767.98; // Переход адаптива с планшета в мобильное состаяние
+```
+
+Разметка:
+
+> app/scss/\_grid.scss
+
+```
+.container {
+	max-width: $maxWidthContainer + px;
+	width: 100%;
+	margin: 0 auto;
+	@media (max-width: ($md1+px)) {
+		max-width: 768px;
+		padding: 0 30px;
+	}
+	@media (max-width: ($md2+px)) {
+		max-width: 100%;
+		padding: 0 10px;
+	}
+}
+```
+
+В дальнейшем медеа запросы можно использовать с переменными
+
+```
+@media (max-width: ($md2+px)) // {} 1200 - 768
+@media (max-width: ($md2+px)) // {} 768 - 320
+```
